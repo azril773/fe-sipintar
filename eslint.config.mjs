145373bf6985e050de144634ea8c4071e78aaa9c
Mohammad Azril Sugiarto <azrilsugiarto06@gmail.com>
2026-03-12 08:01:25 +0700

    feat: initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,72 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from '@eslint/js';
+import json from '@eslint/json';
+import {defineConfig} from 'eslint/config';
+import importPlugin from "eslint-plugin-import";
+import reactHookLint from "eslint-plugin-react-hooks";
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  {
+    
+    ignores: ['src/components/catalyst-ui/**/*.{js,ts,tsx}', 'src/components/global/**/*.{js,ts,tsx}', 'src/components/time-picker/**/*.{js,ts,tsx}', 'src/components/ui/**/*.{js,ts,tsx}'],
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,tsx}'],
+    plugins: {
+      js,
+      'unused-imports': unusedImports,
+      'import': importPlugin,
+      "eslint-plugin-react-hooks": reactHookLint
+    },
+    extends: ['js/recommended'],
+    rules: {
+      "eslint-plugin-react-hooks/exhaustive-deps": "error",
+      'no-unused-vars': 'off',
+      "no-prototype-builtins": "off",
+      "no-undef":"off",
+      'unused-imports/no-unused-imports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_',  },
+      ],  
+      '@typescript-eslint/naming-convention':[
+        'error',
+        {
+          'selector': 'variableLike',
+          'format': ['camelCase', 'UPPER_CASE', "PascalCase"],
+          'leadingUnderscore': 'allow'
+        },
+        {
+          'selector': 'function',
+          'format': ['camelCase', 'PascalCase'],
+        }
+      ],
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'type'],
+        pathGroups: [{ pattern: '@/**', group: 'internal', position: 'after' }],
+        pathGroupsExcludedImportTypes: ['builtin']
+        }
+      ],
+      "sort-imports": [
+        "error",
+        {
+          "ignoreDeclarationSort": true,
+          "ignoreCase": true,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: false,
+        }
+      ],
+    },
+  },
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+  { files: ['**/*.jsonc'], plugins: { json }, language: 'json/jsonc', extends: ['json/recommended'] },
+  { files: ['**/*.json5'], plugins: { json }, language: 'json/json5', extends: ['json/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.browser } },
 ]);
-
-export default eslintConfig;
