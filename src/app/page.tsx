521445e0385e7a2cd3ae411c72da7e21f4c23714
Mohammad Azril Sugiarto <azrilsugiarto06@gmail.com>
2026-04-04 12:52:27 +0700

    feat: enhance layout with GlobalProvider and ToastContainer;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import Image from "next/image";
+
+import { login, refreshToken } from "./_api/auth";
 
 export default function Home() {
   return (
+    
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
@@ -50,6 +55,8 @@ export default function Home() {
             />
             Deploy Now
           </a>
+          <button onClick={() => login('azril@gmail.com', '123')}>Login</button>
+          <button onClick={() => refreshToken()}>Refresh Token</button>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
